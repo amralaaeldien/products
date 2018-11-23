1534ce2f3d5f863efc8bb49800602539a7a65bfe
amrshedou <amrshedou@gmail.com>
2018-11-23 11:07:19 +0200

    third commit

--- products_and_categories/serializers.py
+++ products_and_categories/serializers.py
@@ -11,38 +11,16 @@ class CategorySerializer(serializers.ModelSerializer):
 		if 'categories' not in self.fields:
 			self.fields['categories'] = CategorySerializer(obj, many=True)      
 		return super(CategorySerializer, self).to_representation(obj)
-	
-'''	def to_internal_value(self, data):
-		try:
-			try:
-				obj_id = data['id']
-				return Category.objects.get(id=obj_id)
-			except KeyError:
-				raise serializers.ValidationError(
-					'id is a required field.'
-				)
-			except ValueError:
-				raise serializers.ValidationError(
-					'id must be an integer.'
-				)
-		except Category.DoesNotExist:
-			raise serializers.ValidationError(
-			'Obj does not exist.'
-			)
-'''
+
 	
 class ProductSerializer(serializers.ModelSerializer):
-	categories = CategorySerializer(many=True)#, queryset= Category.objects.all(), slug_field='categories')
-#	categories = CreatableSlugRelatedField(many=False,
-#											queryset=Category.objects.all(),
-#											slug_field='categories',
-#											required=False)
+	categories = CategorySerializer(many=True)
+
 	class Meta:
 		model = Product
 		fields = ('id', "product_code", "name", "quantity", "price", 'categories')
 
 	def update(self, instance, validated_data):
-		#category_data = validated_data.pop('categories')
 
 		instance.product_code = validated_data.get('product_code', instance.product_code)
 		instance.name = validated_data.get('name', instance.name)
@@ -55,11 +33,9 @@ class ProductSerializer(serializers.ModelSerializer):
 
 	def create(self, validated_data):
 		category_data = validated_data.get('categories')
-		id_get = validated_data.get('id')
-		id_pop = validated_data.pop('id')
-		product = Product.objects.get_or_create(id=id_pop, **validated_data)
+		product = Product.objects.create(**validated_data)
 		for category in category_data:
-			product.categories.get_or_create(**category)
+			product.categories.create(**category)
 		return product
 
 
